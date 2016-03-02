1e113b0c9e050b32fd0820a3101da638e8c1522c
Randy Olson <olsonran@msu.edu>
2016-03-02 15:18:57 -0500

    Add a couple more projects to the whitelist
    Looking to add a couple more of my projects to the whitelist. Thank you @bboe!

--- update_checker_app/controllers.py
+++ update_checker_app/controllers.py
@@ -8,7 +8,7 @@ from .helpers import (get_current_version, normalize, record_check,
 from .models import Installation, Package, PythonVersion
 
 
-ALLOWED_PACKAGES = {'lazysusan', 'praw', 'redditanalysis', 'tpot'}
+ALLOWED_PACKAGES = {'lazysusan', 'praw', 'redditanalysis', 'tpot', 'xrff2csv', 'datacleaner'}
 
 
 @APP.route('/')
